fed9d88fd9c753fdd510cd7b76f5a5180b3081d7
Renato Araujo <rkaraujo@gmail.com>
2015-03-07 16:41:19 -0300

    A simple design for the post list page

--- src/main/java/com/rkaraujo/blog/admin/controller/AdminController.java
+++ src/main/java/com/rkaraujo/blog/admin/controller/AdminController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/admin")
 public class AdminController {
 
-	@RequestMapping(value = "/index.html", method = RequestMethod.GET)
+	@RequestMapping(value = { "/", "/index.html" }, method = RequestMethod.GET)
 	public String index() {
 		return "/admin/index";
 	}
