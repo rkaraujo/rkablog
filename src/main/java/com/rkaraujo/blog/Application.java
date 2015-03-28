1cdbe66e7c035942f3e663c8024927c7cd617abc
Renato Araujo <rkaraujo@gmail.com>
2015-03-28 09:04:33 -0300

    Packaging as war

--- src/main/java/com/rkaraujo/blog/Application.java
+++ src/main/java/com/rkaraujo/blog/Application.java
@@ -2,6 +2,8 @@ package com.rkaraujo.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 import com.rkaraujo.blog.config.WebConfig;
@@ -9,7 +11,12 @@ import com.rkaraujo.blog.config.WebSecurityConfig;
 
 @SpringBootApplication
 @Import({ WebSecurityConfig.class, WebConfig.class })
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
